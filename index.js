52ff1bceda92ab51dbb21ab5d9f83ee32af30dc7
X-fherd <x.fherdinand@gmail.com>
2023-08-05 22:49:32 +0000

    Update

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress: req.socket.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent']});
+  res.json({ 
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers['accept-language'], 
+    software: req.headers['user-agent']});
 });
 
 // listen for requests :)
